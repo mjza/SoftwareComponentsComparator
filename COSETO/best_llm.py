8bf3ec84f993901aee27d754277f20b4be0d7e1e
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2025-07-23 22:22:58 -0600

    Add more models

--- COSETO/best_llm.py
+++ COSETO/best_llm.py
@@ -43,11 +43,13 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 models = [
     'meta-llama/Llama-3.1-8B-Instruct',
     'meta-llama/Meta-Llama-3-8B-Instruct',
+    'meta-llama/Llama-2-70b-chat-hf',
+    'tiiuae/falcon-40b-instruct',
     'tiiuae/falcon-7b-instruct',
     'mistralai/Mistral-7B-Instruct-v0.3',
     'deepseek-ai/deepseek-coder-6.7b-instruct',
-    "microsoft/Phi-3-mini-4k-instruct",
-    "Qwen/Qwen1.5-7B-Chat",
+    'microsoft/Phi-3-mini-4k-instruct',
+    'Qwen/Qwen1.5-7B-Chat', 
 ]
 
 # Function to fetch data from the database
